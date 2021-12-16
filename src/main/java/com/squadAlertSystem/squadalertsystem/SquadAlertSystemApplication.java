cb0d783717c91d40f2f6087a5477ee45656dc0ef
Sandesh Bhujbal <sandeshbhujbal@Sandeshs-MacBook-Pro.local>
2021-12-16 20:12:51 +0530

    Adding inital project

---
+++ src/main/java/com/squadAlertSystem/squadalertsystem/SquadAlertSystemApplication.java
@@ -0,0 +1,13 @@
+package com.squadAlertSystem.squadalertsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquadAlertSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquadAlertSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/squadAlertSystem/squadalertsystem/SquadAlertSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squadAlertSystem.squadalertsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquadAlertSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
