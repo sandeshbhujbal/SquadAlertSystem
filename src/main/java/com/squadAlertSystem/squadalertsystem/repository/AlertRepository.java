455003c480e029547d5be0d0cdf0c8144ee534a4
Sandesh Bhujbal <sandeshbhujbal@Sandeshs-MacBook-Pro.local>
2021-12-18 00:09:49 +0530

    Adding status filter in alerts

--- src/main/java/com/squadAlertSystem/squadalertsystem/repository/AlertRepository.java
+++ src/main/java/com/squadAlertSystem/squadalertsystem/repository/AlertRepository.java
@@ -1,5 +1,6 @@
 package com.squadAlertSystem.squadalertsystem.repository;
 
+import com.squadAlertSystem.squadalertsystem.constant.Status;
 import com.squadAlertSystem.squadalertsystem.entity.Alert;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,4 +9,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AlertRepository extends JpaRepository<Alert, String> {
 
     Page<Alert> findAllBySquad(String squad, Pageable pageable);
+
+    Page<Alert> findAllBySquadAndStatus(String squad, Status status, Pageable pageable);
+
+    Page<Alert> findAllByStatus(Status status, Pageable pageable);
 }
