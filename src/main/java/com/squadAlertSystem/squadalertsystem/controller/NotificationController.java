3205e64f1500da53faf7c03d83d97343242541f6
Sandesh Bhujbal <sandeshbhujbal@Sandeshs-MacBook-Pro.local>
2021-12-19 10:10:06 +0530

    Added crossorgin annotation in all controllers

--- src/main/java/com/squadAlertSystem/squadalertsystem/controller/NotificationController.java
+++ src/main/java/com/squadAlertSystem/squadalertsystem/controller/NotificationController.java
@@ -5,6 +5,7 @@ import com.squadAlertSystem.squadalertsystem.constant.Status;
 import com.squadAlertSystem.squadalertsystem.entity.Notification;
 import com.squadAlertSystem.squadalertsystem.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/notification")
+@CrossOrigin
 public class NotificationController {
 
     @Autowired
